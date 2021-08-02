17cf46c0914fc5935344bfb5f7c00972d3c5dea2
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-03 03:40:04 +0600

    shorthands

---
+++ math-shorthand.js
@@ -0,0 +1,36 @@
+//Operators +, -, *, /
+
+var price1 = 31;
+var price2 = 10;
+var sum = price1 + price2;
+var differance = price1 - price2;
+var multiplication = price1 * price2;
+var division = price1 / price2;
+console.log(sum);
+console.log(differance);
+console.log(multiplication);
+console.log(division);
+
+
+//Shorthands
+
+var doublePrice = price2 * 20;
+// var newPrice = price1 + 10;
+// price1 = price1 + 10;
+price1 += 44;
+// price2 = price2 - 10;
+price2 -= 10;
+console.log(price1);
+console.log(price2);
+
+var age = 14;
+// age = age + 1;
+// age += 1;
+age++;
+console.log(age);
+
+var love = 100;
+// love = love - 1;
+// love -= 1;
+love--;
+console.log(love);
