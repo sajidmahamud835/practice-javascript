b3dd7ebd75698c73b5fa2b11649ec948fb0d2306
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-04 19:29:03 +0600

    array

---
+++ array.js
@@ -0,0 +1,13 @@
+var friendAge = [11, 13, 17];
+
+var friendName = ['Mahi', 'Galib', 'Evan', 'Shadin']; //Array
+
+var oddNumbers = [1, 3, 5, 7, 9];
+
+var vowels = ['a', 'e', 'f', 'g', 'h', 'i']
+
+console.log(friendName);
+
+// console.log(friendName.length);
+var vowelsCount = vowels.length;
+console.log(vowelsCount);
