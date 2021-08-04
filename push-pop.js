8962123649b50acca8916681f0d11359803e6e03
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-05 01:21:10 +0600

    comparison-variable

--- push-pop.js
+++ push-pop.js
@@ -3,6 +3,21 @@ lastBench.push('jalam');
 lastBench.push('Shadin');
 // console.log(lastBench);
 
+// add an element to an array
 var friendAge = [11, 12, 13, 14, 15];
 friendAge.push(16);
+// console.log(friendAge);
+
+
+// remove an element (last element) from an array
+// friendAge.pop();
+var lastItem = friendAge.pop();
+// console.log(friendAge);
+
+//remove first item from the array.
+var firstItem = friendAge.shift()
+// console.log(friendAge);
+
+// add element in the beginning.
+friendAge.unshift('Batman');
 console.log(friendAge);
