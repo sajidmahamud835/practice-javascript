1d054ac0bbb5c36bbb8797b7f630427e489f4e4b
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-07 00:08:08 +0600

    facing problem.. while debugging it opens loader.js and other library files.

---
+++ array-loop.js
@@ -0,0 +1,15 @@
+// array
+var numbers = [45, 56, 55, 63, 66, 68, 64, 67];
+
+//show all index of the array using loop.
+// // for loop
+// for (var i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+// while loop
+var i = 0;
+while (i >= numbers.length) {
+    console.log(numbers[i]);
+    i++;
+}
