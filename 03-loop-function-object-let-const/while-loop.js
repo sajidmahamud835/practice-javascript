366b7718fe6d41515dc7231a4cbf20a2ac1098d3
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-06 23:40:40 +0600

    while loop, for loop

---
+++ 03-loop-function-object-let-const/while-loop.js
@@ -0,0 +1,48 @@
+// // while loop
+// var condition = true;
+// var cicrleIndex = 0;
+// while (condition) {
+//     if (cicrleIndex >= 1000) {
+//         condition = false;
+//     }
+//     console.log('Number of loop' + ' ' + cicrleIndex)
+//     cicrleIndex++;
+// }
+
+// var rostGiven = 0;
+
+// while (rostGiven < 7) {
+//     console.log('Give rost please!');
+//     rostGiven++;
+//     console.log(rostGiven);
+// }
+
+// Example
+// loop 1 to 10
+// var number = 1; //loop variable
+
+// while (number <= 10) {
+//     console.log(number);
+//     number++;
+// }
+
+//loop only even number 1-10
+number = 0; //even numbers
+// number = 1; //odd numbers
+while (number <= 20) {
+    console.log(number);
+    number = number + 2;
+}
+
+// var i = 0;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// var num = parseInt(Math.random() * 5);
+// while (num >= 3) {
+//     console.log(num);
+//     num = parseInt(Math.random() * 5);
+// }
+
---
+++ for-loop.js
@@ -0,0 +1,26 @@
+// console.log('running a for loop');
+
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+//     console.log('Give roast please!');
+//     console.log(roastGiven);
+// }
+
+for (var i = 0; i <= 7; i++) {  //for for-loop naming are considerable
+    console.log(i);
+}
+
+// for (var i = 5; i >= 0; i--) {
+//     console.log(i);
+// }
+
+// // odd numbers
+// // i+=2
+// // i = 1 + 2
+// for (var i = 1; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// // even numbers
+// for (var i = 0; i <= 20; i += 2) {
+//     console.log(i);
+// }
