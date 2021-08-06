b57c81f60165f0010cb662eda52d2bb7bc9cef4c
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-07 01:35:44 +0600

    function

--- 04-function-object/function-pramitar-return.js
+++ 04-function-object/function-pramitar-return.js
@@ -1,18 +1,18 @@
 
 // cook food function
-function cookFood() {
+function cookFood(menu) {
     console.log('Cope vagitable');
     console.log('Boil rice');
-    console.log('fry fish');
+    console.log('fry' + ' ' + menu);
 }
 
 //Life of a javascript programmer
 
 console.log('Woke Up');
-cookFood();
+cookFood('fish');
 console.log('code and eat breakfast');
-cookFood();
+cookFood('chicken');
 console.log('code and eat lanch');
-cookFood();
-console('go to sleep');
-
+var item = beef;
+cookFood('beef');
+console.log('go to sleep');
---
+++ 04-function-object/function.js
@@ -0,0 +1,18 @@
+// cook food function
+//function diclaration
+function cookFood() {
+    console.log('Cope vagitable');
+    console.log('Boil rice');
+    console.log('fry fish');
+}
+
+// Life of a javascript programmer
+
+console.log('Woke Up');
+cookFood(); //call function
+console.log('code and eat breakfast');
+cookFood();
+console.log('code and eat lanch');
+cookFood();
+console.log('go to sleep');
+
