594b4912a236737f81afb48b766f7382c153b3f1
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-08 01:52:31 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,37 @@
+var color = 'yollow';
+// if (color == 'blue') {
+//     console.log('color is blue');
+// }
+// else if (color == 'red') {
+//     console.log('color is red');
+// }
+// else if (color == 'green') {
+//     console.log('color is green');
+// }
+// else if (color == 'yollow') {
+//     console.log('color is yollow');
+// }
+// else if (color == 'red') {
+//     console.log('color is white');
+// }
+// else {
+//     console.log('color is unknowen');
+// }
+
+switch (color) {
+    case 'blue':
+        console.log('color is blue');
+        break;
+    case 'white':
+        console.log('color is white');
+        break;
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'yollow':
+        console.log('color is yollow');
+        break;
+    default:
+        console.log('color is blue');
+        break;
+}
