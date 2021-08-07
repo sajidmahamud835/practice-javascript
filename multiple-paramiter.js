510a70115ee7a237497b8579b3d811c95021c962
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-07 18:57:54 +0600

    Multiple paramiter

---
+++ multiple-paramiter.js
@@ -0,0 +1,33 @@
+function addTwoNumbers(number1, number2) {
+    console.log(number1, number2);
+    var total = number1 + number2;
+    return total;
+}
+
+var firstNumber = 21
+var secondNumber = 53
+
+//Multiplication
+function multiplyTwoNumbers(num1, num2) {
+    var result = num1 * num2;
+    return result;
+}
+
+//Substraction
+function subtractNumbers(num1, num2) {
+    var result = num1 - num2;
+    return result;
+}
+
+//Division (Get moduls)
+function devidNumbers(num1, num2) {
+    var result = num1 % num2;
+    return result;
+}
+
+var firstNumber = 200;
+var secondNumber = 53;
+// var resultPreview = addTwoNumbers(firstNumber, secondNumber);
+// var resultPreview = subtractNumbers(firstNumber, secondNumber);
+// var resultPreview = devidNumbers(firstNumber, secondNumber);
+console.log(resultPreview);
