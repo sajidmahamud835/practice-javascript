c5089618b99f55f2e1a964779912ab43654e388a
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-09 03:38:01 +0600

    update

--- problem-solving/basic-problem-solving.js
+++ problem-solving/basic-problem-solving.js
@@ -5,10 +5,10 @@ function seerToMon(seer) {
 
     // input validation
     if (typeof seer != "number") {
-        return 'Invalid input. Your input must be a number.'
+        return 'Invalid input. Your input must be a number.';
     }
     else if (seer < 0) {
-        return 'Invalid input. Seer cannot be a negitive number.'
+        return 'Invalid input. Seer cannot be a negitive number.';
     }
 
     // convertor
@@ -23,10 +23,10 @@ function seerToMon(seer) {
 function totalSales(shirtQuantity, pantQuantity, shoeQuantity) {
     // input validation
     if ((typeof shirtQuantity != "number") || (typeof pantQuantity != "number") || (typeof shoeQuantity != "number")) {
-        return 'Invalid input. Your input must be a number.'
+        return 'Invalid input. Your input must be a number.';
     }
     else if ((shirtQuantity < 0) || (pantQuantity < 0) || (shoeQuantity < 0)) {
-        return 'Invalid input. Price cannot be a negitive number.'
+        return 'Invalid input. Price cannot be a negitive number.';
     }
 
     // item price value
@@ -50,10 +50,10 @@ function totalSales(shirtQuantity, pantQuantity, shoeQuantity) {
 function deliveryCost(quantity) {
     // input validation
     if (typeof (quantity) != "number") {
-        return 'Invalid input. Your input must be a number.'
+        return 'Invalid input. Your input must be a number.';
     }
     else if (quantity < 0) {
-        return 'Invalid input. Quantity cannot be a negitive number.'
+        return 'Invalid input. Quantity cannot be a negitive number.';
     }
 
     //delivery charges
@@ -88,31 +88,39 @@ function deliveryCost(quantity) {
 // (1st one who has 5 cherecters in his/her name is the perfectFriend)
 
 function perfectFriend(friendNames) {
-    // input validation
-    if (typeof friendNames != "array") {
-
+    for (let i = 0; i < friendNames.length; i++) {
+        if ((friendNames[i].length == 5) && (typeof friendNames[i] == "string")) { //also checks if the type is string
+            const perfectDude = friendNames[i];
+            return perfectDude;
+        }
     }
-    return;
+    return 'No perfect friend found.'
 }
 
 
 // testing output
 
 // console.log(seerToMon(40)); // output should be 1
-// console.log(seerToMon(-40)); // output should be invalid message
+// console.log(seerToMon(-40)); // output should be an invalid message
 // console.log(seerToMon(40.50)); //output should be 1.0125
 // console.log(seerToMon(5045)); //output should be 126.125
-// console.log(seerToMon('me')); // output should be invalid message
+// console.log(seerToMon('me')); // output should be an invalid message
 
 // console.log(totalSales(10, 25, 28)); // output should be 20000
-// console.log(totalSales(10, 25, -28)); // output should be invalid message.
+// console.log(totalSales(10, 25, -28)); // output should be an invalid message.
 // console.log(totalSales(0, 0, 0)); // output should be 0.
-// console.log(totalSales(true, 25, 28)); // output should be invalid message.
-// console.log(totalSales(10, 'fifty', 28)); // output should be invalid message.
+// console.log(totalSales(true, 25, 28)); // output should be an invalid message.
+// console.log(totalSales(10, 'fifty', 28)); // output should be an invalid message.
 
 
 // console.log(deliveryCost(10)); //output should be 1000
 // console.log(deliveryCost(110)); //output should be 10800
 // console.log(deliveryCost(10)); //output should be 1000
-// console.log(deliveryCost("free")); //output should be a invalid message.
-// console.log(deliveryCost(-100)); //output should be a invalid message.
+// console.log(deliveryCost("free")); //output should be an invalid message.
+// console.log(deliveryCost(-100)); //output should be an invalid message.
+
+// let friends = ['saju', 'galu', 'omor', 'rakib'];
+// console.log(perfectFriend(friends)); //output should be rakib
+// let fakeFriends = ['saju', 'galu', 'omor', '0'];
+// console.log(perfectFriend(fakeFriends)); //output should be 'not found'.
+// console.log(perfectFriend(['Sajid', 'Galib', 'Mahir', 'Isr'])); //output should be sajid
